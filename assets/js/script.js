d2ff50abe4c84385073515980c2d0a560eb932ae
michelleconville <michelle.conville@gmail.com>
2022-12-13 22:56:28 +0000

    Add function to flip the cards

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const cards = document.querySelectorAll('.memory-card');
+
+/**
+ * Function to flip cards
+ */
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+      hasFlippedCard = true;
+      firstCard = this;
+    }
+}
+
+cards.forEach(card => card.addEventListener('click', flipCard));
