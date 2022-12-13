c638222021b1edaa6c1971eccace08db8dab52df
michelleconville <michelle.conville@gmail.com>
2022-12-13 23:07:33 +0000

    Add function to match the cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,39 @@
 const cards = document.querySelectorAll('.memory-card');
 
-/**
- * Function to flip cards
- */
 let hasFlippedCard = false;
 let firstCard, secondCard;
 
+// Function to flip the cards
 function flipCard() {
-    this.classList.add('flip');
+  this.classList.add('flip');
 
-    if (!hasFlippedCard) {
-      hasFlippedCard = true;
-      firstCard = this;
-    }
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+   return;
+ }
+
+ secondCard = this;
+ hasFlippedCard = false;
+
+ checkForMatch();
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+ firstCard.removeEventListener('click', flipCard);
+ secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+ setTimeout(() => {
+   firstCard.classList.remove('flip');
+   secondCard.classList.remove('flip');
+ }, 1500);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
